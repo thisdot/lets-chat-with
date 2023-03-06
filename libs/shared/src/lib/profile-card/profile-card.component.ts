69c7454d4b5a53c8bf1efa65c04330dd9ae26d3e
morgnism <morgan@border.dev>
2023-03-06 15:34:23 -0700

    feat: split attendee username from social url

--- libs/shared/src/lib/profile-card/profile-card.component.ts
+++ libs/shared/src/lib/profile-card/profile-card.component.ts
@@ -1,5 +1,16 @@
-import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Attendee } from '@conf-match/api';
+import { getSocialProfile } from '@conf-match/utilities';
+import { map, Subject, takeUntil } from 'rxjs';
+import { CmBreakpoints } from '../breakpoint/breakpoint.types';
 
 @Component({
   selector: 'cm-profile-card',
@@ -7,7 +18,15 @@ import { Attendee } from '@conf-match/api';
   styleUrls: ['./profile-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
+  socials = {
+    linkedin: {},
+    twitter: {},
+    facebook: {},
+  };
+  isMobile: boolean = false;
+  private readonly destroy$ = new Subject<void>();
+
   @Input()
   attendee?: Attendee;
 
@@ -22,4 +41,42 @@ export class ProfileCardComponent {
 
   @Output()
   readonly edit = new EventEmitter<void>();
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe(CmBreakpoints.MD.DOWN)
+      .pipe(
+        takeUntil(this.destroy$),
+        map((x) => x?.matches)
+      )
+      .subscribe((x) => {
+        this.isMobile = x;
+      });
+  }
+
+  ngOnInit() {
+    const { linkedin, twitter, facebook } = this.attendee!;
+    this.showSocials = !!linkedin || !!twitter || !!facebook;
+    this.socials = [linkedin, twitter, facebook].reduce(
+      (a, socialURL) => {
+        if (!socialURL) {
+          return a;
+        }
+        const { profileName, sanitizedURL, username } = getSocialProfile(socialURL, this.isMobile);
+
+        return {
+          ...a,
+          [profileName]: {
+            socialURL: sanitizedURL,
+            username,
+          },
+        };
+      },
+      {
+        linkedin: {},
+        twitter: {},
+        facebook: {},
+      }
+    );
+  }
 }
