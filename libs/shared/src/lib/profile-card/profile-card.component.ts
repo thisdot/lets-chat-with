7f27278ccaf720f1fbb32e90a5b92eae760ebb91
Morgan Worrell <morgan@thisdot.co>
2023-03-08 18:36:22 -0500

    Fix/23 profile social links (#49)
    * feat: added social links and username validator package
    
    * feat: added social links and username validator
    
    * feat: added patch functions for form inputs defaults and saved values
    
    * feat: split attendee username from social url
    
    * feat: added sanitization to social inputs
    
    * feat: fixed broken tests
    
    * fix: updates santizer to handle non-url inputs
    
    * fix: reverted edit and input components
    
    * fix: updated social link generator
    
    * fix: reverted input unit tests and component
    
    * feat: minor cleanup

--- libs/shared/src/lib/profile-card/profile-card.component.ts
+++ libs/shared/src/lib/profile-card/profile-card.component.ts
@@ -1,5 +1,22 @@
-import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Attendee } from '@conf-match/api';
+import { getSocialLink } from '@conf-match/utilities';
+import { map, Subject, takeUntil } from 'rxjs';
+import { CmBreakpoints } from '../breakpoint/breakpoint.types';
+
+const SOCIALS_DEFAULT = {
+  linkedin: {},
+  twitter: {},
+  facebook: {},
+};
 
 @Component({
   selector: 'cm-profile-card',
@@ -7,7 +24,11 @@ import { Attendee } from '@conf-match/api';
   styleUrls: ['./profile-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
+  socials = SOCIALS_DEFAULT;
+  isMobile: boolean = false;
+  private readonly destroy$ = new Subject<void>();
+
   @Input()
   attendee?: Attendee;
 
@@ -22,4 +43,38 @@ export class ProfileCardComponent {
 
   @Output()
   readonly edit = new EventEmitter<void>();
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe(CmBreakpoints.MD.DOWN)
+      .pipe(
+        takeUntil(this.destroy$),
+        map((x) => x?.matches)
+      )
+      .subscribe((x) => {
+        this.isMobile = x;
+      });
+  }
+
+  ngOnInit() {
+    const { linkedin, twitter, facebook } = this.attendee!;
+    const socials = { linkedin, twitter, facebook };
+
+    this.showSocials = Object.values(socials).some((x) => !!x);
+    this.socials = Object.keys(socials).reduce((a, profileName) => {
+      const username = socials[profileName];
+      if (!username) {
+        return a;
+      }
+      const sanitizedURL = getSocialLink(profileName, username, this.isMobile);
+
+      return {
+        ...a,
+        [profileName]: {
+          socialURL: sanitizedURL,
+          username,
+        },
+      };
+    }, SOCIALS_DEFAULT);
+  }
 }
